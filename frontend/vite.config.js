38ef5a83a444a1f669684b893bb086bc922ba812
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-09-17 21:16:55 +0600

    Installed react dom

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,5 +11,9 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+    "/sanctum": {
+      target: "http://127.0.0.1:8000",
+      changeOrigin: true,
+    },
   },
 });
